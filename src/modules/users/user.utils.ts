80b891e665366fc5b35cdb08092c8623bf9ba78d
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2023-06-02 12:34:05 +0600

    done

--- src/modules/users/user.utils.ts
+++ src/modules/users/user.utils.ts
@@ -1,15 +1,15 @@
 import { User } from './user.model';
 
 const findLastId = async () => {
-  const lastId = await User.findOne({}, { id: 1, _id: -1 })
+  const lastId = await User.findOne({}, { id: 1, _id: 0 })
     .sort({ createdAt: -1 })
     .lean();
-  return lastId;
+  return lastId?.id;
 };
 
 export const generateUserID = async () => {
   const lastId = await findLastId();
   const nextID = Number(lastId) + 1;
-  const paddedID = `${nextID}`.padStart(6, '0');
+  const paddedID = `${nextID}`.padStart(6, '0') || '000000';
   return paddedID;
 };
