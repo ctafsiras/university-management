8f742fea85dd0caae1ad3845cf4f52b34f25abdc
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2023-06-02 12:28:20 +0600

    [DONE] service

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,5 @@ dotenv.config();
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
-  defaultStudentPassword: process.env.DEFAULT_STUDENT_PASSWORD,
+  defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
 };
--- src/modules/users/user.utils.ts
+++ src/modules/users/user.utils.ts
@@ -1,9 +1,6 @@
-//generate userid from mongoose model, take last id and increment by 1 and id fixed length 6
-
 import { User } from './user.model';
 
 const findLastId = async () => {
-  // Get the last ID in the model.
   const lastId = await User.findOne({}, { id: 1, _id: -1 })
     .sort({ createdAt: -1 })
     .lean();
@@ -12,9 +9,7 @@ const findLastId = async () => {
 
 export const generateUserID = async () => {
   const lastId = await findLastId();
-  const nextID = lastId + 1;
-
+  const nextID = Number(lastId) + 1;
   const paddedID = `${nextID}`.padStart(6, '0');
-
   return paddedID;
 };
