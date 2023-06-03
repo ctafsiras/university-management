192c3f3f7ca08bcb277596e7a390a94fc8792de1
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2023-06-03 07:25:41 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
 import userRouter from './modules/users/user.route';
 const app: Application = express();
 
